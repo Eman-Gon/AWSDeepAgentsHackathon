acf4bc54b0daa1fef07d980e61707fd4a050c6f7
Kevin Chen <kvn.c8888@gmail.com>
2026-03-27 14:16:15 -0700

    fix: enable scrolling in right sidebar panels (min-height: 0 + overflow)

--- homepage/dist/assets/globe.gl-BXkBNzNh.js
+++ homepage/dist/assets/globe.gl-BXkBNzNh.js
@@ -1,4 +1,4 @@
-import{t as h7,Q as XE,J as f7}from"./index-DN9Z1OUO.js";/**
+import{t as h7,Q as XE,J as f7}from"./index-CMO1TYlW.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
